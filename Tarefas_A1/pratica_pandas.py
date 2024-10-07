ddec6309d52ca22d2550de624375e0f3252b21ce
vini-ps <164961851+vini-ps@users.noreply.github.com>
2024-10-07 01:14:07 -0300

    Update pratica_pandas.py

--- Tarefas_A1/pratica_pandas.py
+++ Tarefas_A1/pratica_pandas.py
@@ -4,7 +4,7 @@ import pandas as pd
 ####1
 df = pd.read_csv("dataset.csv")
 
-####2
+####2.
 print(df.to_string())
 
 ####3
@@ -12,8 +12,22 @@ print(df.to_string())
 filtro_IDH = df[(df['ano']==1991)&(df['idhm'])]
 
 ##(b)
-IDH = pd.Series(filtra['idhm'])
+IDH = pd.Series(filtro_IDH['idhm'])
 
 ##(c)
 print(IDH.mean())
 
+####4
+##(a)
+filtro_mediaIDH = filtro_IDH['idhm'] > IDH.mean()
+filtro_siglauf = filtro_IDH['sigla_uf']
+
+##(b)
+filtro_uf = filtro_IDH[filtro_mediaIDH & filtro_siglauf]
+
+####5
+df_ordenado = df.sort_values('idhm')
+
+df_ordenado.head(5)
+
+####6
