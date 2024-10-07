b5538baec468954650ca1e31cbc6045d02f86c22
vini-ps <164961851+vini-ps@users.noreply.github.com>
2024-10-07 20:37:23 -0300

    Update pratica_numpy.py

--- Tarefas_A1/pratica_numpy.py
+++ Tarefas_A1/pratica_numpy.py
@@ -1,4 +1,5 @@
-import numpy as np
+from numpy import random # type: ignore
+import numpy as np # type: ignore
 import math
 
 lista = np.array([5.5, 3.8, 9, 7.5, 10.0, 9.9, 8.5])
@@ -38,3 +39,11 @@ print(lista.mean())
 desvio_pd = lista.std()
 maximo = lista.max()
 minimo = lista.min()
+
+##(e)
+array_aleatorio = random.randint(11,size=(100))
+
+print(f'media = {array_aleatorio.mean():.2f}')
+print(f'desvio padrao = {array_aleatorio.std():.2f}')
+print(f'maior valor = {array_aleatorio.max()}')
+print(f'menor valor = {array_aleatorio.min()}')
