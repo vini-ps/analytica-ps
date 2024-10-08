7adeb6a1259b5ab08888df18be2e448a86df009e
vini-ps <164961851+vini-ps@users.noreply.github.com>
2024-10-07 21:55:53 -0300

    Update pratica_pandas.py

--- Tarefas_A1/pratica_pandas.py
+++ Tarefas_A1/pratica_pandas.py
@@ -1,5 +1,4 @@
-import pandas as pd
-#Parte Prática 1.2.2
+import pandas as pd # type: ignore
 
 ####1
 df = pd.read_csv("dataset.csv")
@@ -15,19 +14,22 @@ filtro_IDH = df[(df['ano']==1991)&(df['idhm'])]
 IDH = pd.Series(filtro_IDH['idhm'])
 
 ##(c)
-print(IDH.mean())
+print(f'IDH medio de 1991 = {IDH.mean():.3f}\n')
 
 ####4
 ##(a)
 filtro_mediaIDH = filtro_IDH['idhm'] > IDH.mean()
 filtro_siglauf = filtro_IDH['sigla_uf']
 
-##(b)
+##(b) estados com IDH maior que a media calculada na questao 3
 filtro_uf = filtro_IDH[filtro_mediaIDH & filtro_siglauf]
+print(f'{filtro_uf}\n')
 
 ####5
 df_ordenado = df.sort_values('idhm')
+print(df_ordenado.head(5))
 
-df_ordenado.head(5)
-
+print('\n')
 ####6
+print(f"Estado com menor IDH em 1991: \n{filtro_uf['sigla_uf'].min()} indice {filtro_uf['sigla_uf'].idxmin()}")
+print(f"Estado com maior IDH em 1991: \n{filtro_uf['sigla_uf'].max()} indice {filtro_uf['sigla_uf'].idxmax()}")
