a1e2cd67fe1286cf7dd36bb44fa1fc9034b63aa9
vini-ps <164961851+vini-ps@users.noreply.github.com>
2024-10-08 21:55:33 -0300

    Update desafio_final.py

--- Tarefas_A1/desafio_final.py
+++ Tarefas_A1/desafio_final.py
@@ -3,27 +3,35 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from pratica_pandas import *
 
-def diferenca_expect_vida():
+def aumento_expectatita_vida():
     filtro_1991 = df[df['ano']==1991]
     filtro_2010 = df[df['ano']==2010]
 
     expect_vida1991 = np.array(filtro_1991['expectativa_vida'])
     expect_vida2010 = np.array(filtro_2010['expectativa_vida'])
 
+    #print(expect_vida1991)
+    #print(expect_vida2010)
+
     diferenca_expect_vida = np.array([])
     for i in range(len(expect_vida1991)):
         diferenca = round(expect_vida2010[i] - expect_vida1991[i],2) 
         diferenca_expect_vida = np.append(diferenca_expect_vida, diferenca)
-
-    return diferenca_expect_vida
-
-
-def grafico():
-    expect_vida = diferenca_expect_vida()
+    
+    #print(diferenca_expect_vida)
     estados = np.array(filtro_siglauf)
 
-    plt.bar(estados, expect_vida)
+    plt.bar(estados, diferenca_expect_vida)
     plt.show()
+    
+    resposta = []
+    for i in range(len(diferenca_expect_vida)):
+        if diferenca_expect_vida[i] >= 10:
+            resposta.append(estados[i])
+    
+    print(resposta)
 
-    return 
+    return resposta
 
+
+aumento_expectatita_vida()
