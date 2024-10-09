3fb78abb8ab35af8d731d99a7ddbbd5d8551e708
vini-ps <164961851+vini-ps@users.noreply.github.com>
2024-10-08 21:28:08 -0300

    Update desafio_final.py

--- Tarefas_A1/desafio_final.py
+++ Tarefas_A1/desafio_final.py
@@ -1,1 +1,35 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from pratica_pandas import *
+
+def diferenca_expect_vida():
+    filtro_1991 = df[df['ano']==1991]
+    filtro_2010 = df[df['ano']==2010]
+
+    expect_vida1991 = np.array(filtro_1991['expectativa_vida'])
+    expect_vida2010 = np.array(filtro_2010['expectativa_vida'])
+
+    #print(expect_vida1991)
+    #print(expect_vida2010)
+
+    diferenca_expect_vida = np.array([])
+    for i in range(len(expect_vida1991)):
+        diferenca = round(expect_vida2010[i] - expect_vida1991[i],2) 
+        diferenca_expect_vida = np.append(diferenca_expect_vida, diferenca)
+    
+    
+    #print(diferenca_expect_vida)
+
+    return diferenca_expect_vida
+
+
+def grafico():
+    expect_vida = diferenca_expect_vida()
+    estados = np.array(filtro_siglauf)
+
+    plt.bar(estados, expect_vida)
+    plt.show()
+
+    return 
 
